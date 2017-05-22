665a34514931fa97c309c4795d71b9ff41476bc6
yeyou9440 <yeyou9440@163.com>
2017-05-22 19:28:39 +0800

    test commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 --哈罗</div>
+                <div class="title">Laravel 5 --哈罗0522</div>
             </div>
         </div>
     </body>
